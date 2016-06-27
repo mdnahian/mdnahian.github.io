24c599aac6e3ef57fa16549353b0828b08820de4
Md Islam <mdnahian@outlook.com>
2016-06-27 13:33:13 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var server = require('diet');
+var app = server();
+
+var ect = require('ect')({ root  : app.path+'/views/html/', watch: true, ext: '.html' });
+app.view('html', ect.render);
+
+var stat  = require('diet-static')({ path: app.path+'/views/' });
+app.view('file', stat);
+
+app.listen('http://mdislam.com/');
+
+app.get('/', function($){
+	$.end();
+});
+
+
+app.get('/privacy', function($){
+	$.end();
+});
+
+app.get('/terms', function($){
+	$.end();
+});
+
+app.get('/feedback', function($){
+	$.end();
+});
