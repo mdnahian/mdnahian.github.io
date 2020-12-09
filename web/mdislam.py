ce0ed308e6a642a41ac04f397bf1f70d462e3cae
Md Islam <mdnahian@outlook.com>
2020-12-09 15:48:16 -0500

    Import app instead of module

--- web/mdislam.py
+++ web/mdislam.py
@@ -4,5 +4,5 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/opt/www/mdislam.com/")
 
-from rest import app as application
+from rest.app import app as application
 application.secret_key = 'mdislam'
