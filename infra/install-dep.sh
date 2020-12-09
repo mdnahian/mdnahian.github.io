f69cede903111eed23484861aaed8d785e136445
Md Islam <mdnahian@outlook.com>
2020-12-09 15:00:56 -0500

    Fix permissions

--- infra/install-dep.sh
+++ infra/install-dep.sh
@@ -11,16 +11,20 @@ git clone https://github.com/mdnahian/${APP_NAME}.git ${INSTALL_DIR}/${APP_NAME}
 rm -rf ${INSTALL_DIR}/venv/${APP_NAME}
 mkdir -p ${INSTALL_DIR}/venv
 python3 -m venv ${INSTALL_DIR}/venv/${APP_NAME}
+${INSTALL_DIR}/venv/${APP_NAME}/bin/pip install wheel
 ${INSTALL_DIR}/venv/${APP_NAME}/bin/pip install -r ${INSTALL_DIR}/${APP_NAME}/web/requirements.txt
 
 mkdir -p /var/log/uwsgi
 mv ${INSTALL_DIR}/${APP_NAME}/web/rest.service /etc/systemd/system/mdislam.service
+chown -R www-data:www-data /var/log/uwsgi
+chown -R www-data:www-data ${INSTALL_DIR}
 systemctl start mdislam
 systemctl enable mdislam
 
 mv  ${INSTALL_DIR}/${APP_NAME}/web/rest.nginx /etc/nginx/sites-available/mdislam.com
-ln -s /etc/nginx/sites-available/mdislam.com /etc/nginx/sites-enabled
+ln -sf /etc/nginx/sites-available/mdislam.com /etc/nginx/sites-enabled
 nginx -t
 systemctl restart nginx
+systemctl daemon-reload
 ufw allow 'Nginx Full'
 
