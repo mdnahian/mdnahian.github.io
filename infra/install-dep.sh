6b958b9f29ea2ffaa89b5faafc0280a9e4ce19b8
Md Islam <mdnahian@outlook.com>
2020-12-09 14:09:25 -0500

    Remove venv before creating

--- infra/install-dep.sh
+++ infra/install-dep.sh
@@ -9,6 +9,7 @@ apt update
 apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx
 git clone https://github.com/mdnahian/${APP_NAME}.git ${INSTALL_DIR}/${APP_NAME}
 mkdir -p ${INSTALL_DIR}/venv
+rm -rf ${INSTALL_DIR}/venv/${APP_NAME}
 python3 -m venv ${INSTALL_DIR}/venv/${APP_NAME}
 ${INSTALL_DIR}/venv/${APP_NAME}/bin/pip install -r ${INSTALL_DIR}/${APP_NAME}/web/requirements.txt
 
