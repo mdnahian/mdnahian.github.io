d676fd95e2171730c0c6109cac8227280fa25990
Md Islam <mdnahian@outlook.com>
2021-09-17 21:36:26 -0400

    terraform deploy

--- web/rest/app.py
+++ web/rest/app.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import glob
+import feedparser
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, url_for, send_from_directory, make_response
 
 app = Flask(__name__, static_url_path='')
@@ -11,10 +13,25 @@ def get_travel_photos():
     images = glob.glob(image_path)
     return [url_for('static', filename="images/travel/%s" % os.path.basename(photo)) for photo in images]
 
+def get_blog_posts():
+    blog_posts = []
+    rss_feed_url = "https://medium.com/feed/@mdnahian"
+    rss_feed = feedparser.parse(rss_feed_url)
+    for entry in rss_feed.entries:
+        post = {
+            'title': entry.title,
+            'image': BeautifulSoup(entry.summary).find('img')['src'],
+            'published': entry.published,
+            'link': entry.link
+        }
+        blog_posts.append(post)
+    return blog_posts
+
 
 @app.route('/')
 def index():
-        return render_template('index.html', published_projects=published_projects, hackathon_projects=hackathon_projects, activities=activities, travel_photos=get_travel_photos())
+        return render_template('index.html', published_projects=published_projects, hackathon_projects=hackathon_projects,
+         activities=activities, travel_photos=get_travel_photos(), blog_posts=get_blog_posts())
 
 
 @app.route('/<path:path>')
